00b3504da3dc63aff0a2a61faef11a394a569964
bvo24 <brianvo2020@gmail.com>
2024-05-25 13:13:54 -0400

    -Implemented total -Previous Rolls -Stored on swiftdata -Missing: -hapitc/accessibilities -go through values animation

---
+++ DiceRoller/Dice.swift
@@ -0,0 +1,8 @@
+//
+//  Dice.swift
+//  DiceRoller
+//
+//  Created by Brian Vo on 5/23/24.
+//
+
+import Foundation
---
+++ DiceRoller/PreviousRollsView.swift
@@ -0,0 +1,18 @@
+//
+//  PreviousRollsView.swift
+//  DiceRoller
+//
+//  Created by Brian Vo on 5/23/24.
+//
+
+import SwiftUI
+
+struct PreviousRollsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviousRollsView()
+}
